8222bcdc5716989434812ce1e1e58e11a50353d4
caveat-dev <nathan.wignall06@gmail.com>
2023-10-04 21:00:53 -0500

    Added support for facing argument in Teleport

--- src/dev/caveatemptor/DatapackAPI/Command/Teleport/Teleport.java
+++ src/dev/caveatemptor/DatapackAPI/Command/Teleport/Teleport.java
@@ -5,7 +5,7 @@ import dev.caveatemptor.DatapackAPI.Vector.Vector2;
 import dev.caveatemptor.DatapackAPI.Vector.Vector3;
 
 
-// TODO: Implement facing
+// TODO: Implement facingEntity
 
 /**
  * Represents the teleport command.
@@ -14,6 +14,7 @@ public class Teleport implements dev.caveatemptor.DatapackAPI.Command.Command {
     Target target;
     Vector3 location;
     Vector2 rotation;
+    Vector3 facingLocation;
 
 
     /**
@@ -40,21 +41,23 @@ public class Teleport implements dev.caveatemptor.DatapackAPI.Command.Command {
     }
 
     /**
+     * Creates a new Teleport command with the given location, facing facingLocation.
+     * @param target Defines what entities will be selected as a target.
+     * @param location The location to teleport the target to.
+     * @param facingLocation The location to face towards.
+     */
+    public Teleport(Target target, Vector3 location, Vector3 facingLocation) {
+        this.target = target;
+        this.location = location;
+        this.facingLocation = facingLocation;
+    }
+
+    /**
      * Creates a new Teleport command with the given location. This will only affect the entity executing the command.
      * @param location The location to teleport the target to.
      */
     public Teleport(Vector3 location) {
         this.location = location;
-    }
-
-    /**
-     * Creates a new Teleport command with the given location and rotation. This will only affect the entity executing the command.
-     * @param location The location to teleport the target to.
-     * @param rotation The rotation to set the target to.
-     */
-    public Teleport(Vector3 location, Vector2 rotation) {
-        this.location = location;
-        this.rotation = rotation;
     }
 
 
@@ -67,13 +70,17 @@ public class Teleport implements dev.caveatemptor.DatapackAPI.Command.Command {
         String generated = "teleport ";
 
         if (target != null) {
-            generated += " " + target.toString() + " ";
+            generated += target.generate() + " ";
         }
 
-        generated += location.toString();
+        generated += location.generate();
 
         if (rotation != null) {
-            generated += " " + rotation + " ";
+            generated += " " + rotation.generate() + " ";
+        }
+
+        if (facingLocation != null) {
+            generated += " facing " + facingLocation.generate();
         }
 
         return generated;
